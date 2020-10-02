d6fd48fe8f7ce63e0f457f53001da4bf8fec5b51
Ks5810 <e40keisuke@gmail.com>
2020-10-02 02:37:40 -0400

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+/**
+ * ecosystem.config.js
+ * @author [Keisuke Suzuki](https://github.com/Ks5810)
+ * @description PM2 configuration file which run yarn start
+ */
+
+module.exports = {
+    apps : [{
+        name: 'portfolio',
+        script: 'yarn',
+        cwd: '/home/ec2-user/portfolio',
+        interpreter: '/bin/bash',
+        args: 'start',
+        instances: 1,
+        autorestart: true,
+        max_memory_restart: '512M',
+    }],
+};
