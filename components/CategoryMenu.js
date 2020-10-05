2de957be760e9303e80b320c047ecaffa90eea30
Ks5810 <e40keisuke@gmail.com>
2020-10-05 01:55:23 -0400

    added category

--- components/CategoryMenu.js
+++ components/CategoryMenu.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 import {useRouter} from "next/router";
 import svgSchool from "../public/assets/images/school.svg";
 import svgAlgo from "../public/assets/images/algorithm.svg";
-import svgAll from "../public/assets/images/all.svg"
+import svgAll from "../public/assets/images/all.svg";
+import svgDev from "../public/assets/images/dev.svg";
 
 
 const MyLink = React.forwardRef(({onClick, href, catIcon, catName}, ref) => {
@@ -19,7 +20,6 @@ const MyLink = React.forwardRef(({onClick, href, catIcon, catName}, ref) => {
 })
 
 const CategoryLink = ({catName, catIcon, catLink, path}) => {
-    console.log(path);
     return (
         <div
             className={`category-item ${catLink === path && "active"}`}>
@@ -39,12 +39,12 @@ const CategoryMenu = () => {
             <ul className="category-item-list">
                 <CategoryLink catName="All" catIcon={svgAll} catLink="/"
                               path={path}/>
-                {/*<CategoryLink*/}
-                {/*    catName="Dev"*/}
-                {/*    catIcon={svgDev}*/}
-                {/*    catLink="/categories/dev"*/}
-                {/*    path={path}*/}
-                {/*/>*/}
+                <CategoryLink
+                    catName="Dev"
+                    catIcon={svgDev}
+                    catLink="/categories/dev"
+                    path={path}
+                />
                 <CategoryLink
                     catName="Algorithm"
                     catIcon={svgAlgo}
